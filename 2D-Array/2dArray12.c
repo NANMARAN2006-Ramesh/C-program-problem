d4a84a04a06d56bbfedfd02555e72d832ec901d6
Nanmaran Ramesh <nmaran294@gmail.com>
2026-02-05 21:51:38 +0530

    2d-Array Problem

---
+++ 2D-Array/2dArray12.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    scanf("%d%d", &r, &c);
+    int arr[r][c];
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    int max= 0;
+    int freq = arr[0][0]; 
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            int count = 0;
+            for (int x = 0; x < r; x++) {
+                for (int y = 0; y < c; y++) {
+                    if (arr[i][j] == arr[x][y]) {
+                        count++;
+                    }
+                }
+            }
+            if (count > max) {
+                max = count;
+                freq = arr[i][j];
+            }
+        }
+    }
+    printf("Frequent count is %d", freq);
+    return 0;
+}
